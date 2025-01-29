502f015a80f07ad7c132203380ea17dc254a2b81
Divyesh-Thakur45 <divyeshthakur370@gmail.com>
2025-01-29 11:30:46 +0530

    update

--- zeta-clone/src/routes/AllRoutes.jsx
+++ zeta-clone/src/routes/AllRoutes.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-// import Home from '../views/Home/Hero.jsx'
-import Hero from '../views/home/Home.jsx'
+import { Route, Routes } from 'react-router-dom';
+import Hero from '../views/Home/Hero';
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Hero />} />
-        {/* <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </div>
   )
---
+++ zeta-clone/src/views/Home/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Technology from './Technology';
+import ProcessSlide from './ProcessSlide';
+import Solutaion from './Solutaion';
+import Benefits from './Benefits';
+import Contact from './Contact';
+import Career from './Career';
+const Hero = () => {
+  return (
+    <div>
+      <Technology />
+      <ProcessSlide />
+      <Solutaion />
+      <Benefits />
+      <Contact />
+      <Career />
+    </div>
+  )
+}
+
+export default Hero
